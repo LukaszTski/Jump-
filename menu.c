0f4793361861c5b281e8a57f7a2eef21ce4d472e
LukaszTski <34355985+LukaszTski@users.noreply.github.com>
2017-12-13 15:22:19 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -8,7 +8,6 @@ int main(int argc, char **argv) {
 	ALLEGRO_BITMAP  *image = NULL;
 	ALLEGRO_BITMAP  *image1 = NULL;
 	ALLEGRO_BITMAP  *image2 = NULL;
-	ALLEGRO_BITMAP  *image3 = NULL;
 
 	if (!al_init()) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
@@ -22,7 +21,7 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	display = al_create_display(1366, 768);
+	display = al_create_display(1024, 768);
 
 	if (!display) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
@@ -32,8 +31,7 @@ int main(int argc, char **argv) {
 
 	image = al_load_bitmap("titlee.png");
 	image1 = al_load_bitmap("newgame.png");
-	image2 = al_load_bitmap("options.png");
-	image3 = al_load_bitmap("exit.png");
+	image2 = al_load_bitmap("exit.png");
 
 	if (!image) {
 		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
@@ -42,21 +40,17 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-
-
-	al_draw_bitmap(image, 450, 0, 0);
-	al_draw_bitmap(image1, 0, 250, 0);
-	al_draw_bitmap(image2, 0, 350, 0);
-	al_draw_bitmap(image3, 0, 450, 0);
+	al_draw_bitmap(image, 270, 0, 0);
+	al_draw_bitmap(image1, 0, 300, 0);
+	al_draw_bitmap(image2, 0, 400, 0);
 
 	al_flip_display();
-	al_rest(10);
+	al_rest(9);
 
 	al_destroy_display(display);
 	al_destroy_bitmap(image);
 	al_destroy_bitmap(image1);
 	al_destroy_bitmap(image2);
-	al_destroy_bitmap(image3);
 
 	return 0;
 }
